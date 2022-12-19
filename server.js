9c2e045e622740081499cfed148fcc3f148972be
Anoosh <anooshmangalath@gmail.com>
2022-12-19 11:46:07 +0530

    dep

--- server.js
+++ server.js
@@ -2,18 +2,30 @@ const express = require("express");
 const ConnectDB = require("./config/DBconfig");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
+const path = require("path");
 const catogoriesRoute = require("./routes/catogoriesRoute");
 const productsRoute = require("./routes/productsRoute");
 
 const app = express();
 
-app.use(cors());
 ConnectDB();
 
 app.use(express.json());
 
 app.use("/api/", productsRoute);
 app.use("/api/", catogoriesRoute);
+
+// serving frontend code
+
+app.use(express.static(path.join(__dirname, "./client/dist")));
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./client/dist/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
 
 const PORT = process.env.PORT;
 app.listen(PORT, (req, res) => {
